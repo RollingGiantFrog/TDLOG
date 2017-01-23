1d68318111fcef8cbe647581895b68e0133e87d1
Victor Kani <victor.kani@eleves.enpc.fr>
2017-01-23 14:37:44 +0100

    Formulaire de recherche

--- Site_Frigo/fridgeApp/urls.py
+++ Site_Frigo/fridgeApp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^contact$', views.Contact, name='contact'),
     url(r'^recipe/(?P<recipe_id>[0-9]+)$', views.recipe, name='recipe'),
+    url(r'^search/$', views.search, name='search'),
 ]
--- Site_Frigo/fridgeApp/views.py
+++ Site_Frigo/fridgeApp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 from django.views import generic
 from django.urls import reverse
 
-from .models import Recipe
+from .models import Recipe, Ingredient
 # Create your views here.
 class IndexView(generic.ListView):
     template_name = "fridgeApp/index.html"
@@ -23,3 +23,19 @@ def recipe(request, recipe_id):
     recipe = get_object_or_404(Recipe, pk = recipe_id)
     #return HttpResponseRedirect(reverse('fridgeApp:recipe',args=(recipe.id)))
     return render(request, 'fridgeApp/recipe.html', locals())
+
+
+
+from django import forms
+
+class SearchRecipeForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(SearchRecipeForm, self).__init__(*args, **kwargs)
+        # dynamic fields here ...
+        for i in Ingredient.objects.all():
+            self.fields[i.ingredient_text] = forms.BooleanField(help_text="\n", required=False)
+    # normal fields here ...
+            
+def search(request):
+    form = SearchRecipeForm(request.POST or None)
+    return render(request, 'fridgeApp/search.html', locals())
