609832b3466f1b50829dae5f72c6fb4d4fdb6021
Victor Kani <victor.kani@eleves.enpc.fr>
2017-01-23 16:24:37 +0100

    navigation

--- Site_Frigo/fridgeApp/urls.py
+++ Site_Frigo/fridgeApp/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     url(r'^contact$', views.contact, name='contact'),
     url(r'^recipe/(?P<recipe_id>[0-9]+)$', views.recipe, name='recipe'),
     url(r'^search/$', views.search, name='search'),
-    url(r'^add/$', views.add, name='add'),
+    url(r'^add$', views.add, name='add'),
 ]
