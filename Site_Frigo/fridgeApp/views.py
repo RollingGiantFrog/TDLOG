1b1050cf964542ebe859f57cb5c031cebeb46779
Victor Kani <victor.kani@eleves.enpc.fr>
2017-01-30 17:01:25 +0100

    correction

--- Site_Frigo/fridgeApp/views.py
+++ Site_Frigo/fridgeApp/views.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from operator import itemgetter
-from __future__ import unicode_literals
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.views import generic
